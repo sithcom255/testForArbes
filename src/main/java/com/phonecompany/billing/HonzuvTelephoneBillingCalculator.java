1b555c7790c139b4baa8d3b83f151a6da6e7b1f2
sithcom255 <honza.gavlik@gmail.com>
2022-01-11 18:33:30 +0100

    add dependencies, and basic files

---
+++ src/main/java/com/phonecompany/billing/HonzuvTelephoneBillingCalculator.java
@@ -0,0 +1,11 @@
+package com.phonecompany.billing;
+
+import java.math.BigDecimal;
+
+public class HonzuvTelephoneBillingCalculator implements TelephoneBillCalculator{
+
+    @Override
+    public BigDecimal calculate(String phoneLog) {
+        return null;
+    }
+}

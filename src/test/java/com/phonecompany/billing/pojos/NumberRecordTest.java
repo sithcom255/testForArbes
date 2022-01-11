670cc611499eac257bb8bb10fffbdca68374eaf2
sithcom255 <honza.gavlik@gmail.com>
2022-01-11 20:10:39 +0100

    add first edge case

--- src/test/java/com/phonecompany/billing/pojos/NumberRecordTest.java
+++ src/test/java/com/phonecompany/billing/pojos/NumberRecordTest.java
@@ -12,20 +12,34 @@ import java.time.format.DateTimeFormatter;
 public class NumberRecordTest {
 
     private final SoftAssertions softAssertions = new SoftAssertions();
+    private DateTimeFormatter formatter;
 
     @Test
     public void calculateCallPriceValid() {
-        String startS = "13-01-2020 18:10:15";
-        String endS = "13-01-2020 18:12:57";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
-        LocalDateTime start = LocalDateTime.parse(startS, formatter);
-        LocalDateTime end = LocalDateTime.parse(endS, formatter);
+        LocalDateTime start = LocalDateTime.parse("13-01-2020 18:10:15", formatter);
+        LocalDateTime end = LocalDateTime.parse("13-01-2020 18:12:57", formatter);
+
         NumberRecord callRecord = new NumberRecord(1);
         callRecord.addCall(start, end);
 
         checkCallRecord(callRecord, BigDecimal.valueOf(1.0), Duration.ofSeconds(162));
     }
+
+    @Test
+    public void calculateCallStartingShortlyBeforeHighPrice() {
+        formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        LocalDateTime start = LocalDateTime.parse("13-01-2020 07:58:15", formatter);
+        LocalDateTime end = LocalDateTime.parse("13-01-2020 08:0:15", formatter);
+
+        NumberRecord callRecord = new NumberRecord(1);
+        callRecord.addCall(start, end);
+
+        checkCallRecord(callRecord, BigDecimal.valueOf(1.0), Duration.ofSeconds(162));
+    }
+    
+
 
     private void checkCallRecord(NumberRecord record, BigDecimal price, Duration duration) {
         softAssertions.assertThat(record.getPrice()).isEqualTo(price);
